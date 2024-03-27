1c774de88819726297a4f62ad78073082875ca7b
aliisaro <aliisa.rokala@metropolia.fi>
2024-03-27 13:19:51 +0200

    Orientation tasks 1 task 3 update

--- src/Orientation_tasks1/Task1_3_Customer.java
+++ src/Orientation_tasks1/Task1_3_Customer.java
@@ -10,10 +10,6 @@ public class Task1_3_Customer {
         id = _id++;
     }
 
-    public void tulosta() {
-        System.out.println("Asiakasnumero on " + id);
-    }
-
     void setStartTime(long time) {
         if (time > 0) {
             startTime = time;
@@ -34,15 +30,39 @@ public class Task1_3_Customer {
         return endTime;
     }
 
-    long duration() {
-        return endTime - startTime;
+    double getDuration() {
+        return (endTime - startTime) / 1000.0;
+    }
+
+    private static void viive(int aika) {
+        try {
+            Thread.sleep(aika*1000); //1000ms = 1s
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void tulosta() {
+        System.out.println("Asiakasnumero on " + id + ", aloitusaika " + startTime + ", lopetusaika " + endTime);
     }
 
     public static void main(String[] args) {
-        Task1_3_Customer asiakas1 = new Task1_3_Customer();
-        Task1_3_Customer asiakas2 = new Task1_3_Customer();
-        Task1_3_Customer asiakas3 = new Task1_3_Customer();
 
-        asiakas1.tulosta(); asiakas2.tulosta(); asiakas3.tulosta();
+        //Customer 1
+        Task1_3_Customer asiakas1 = new Task1_3_Customer();
+        asiakas1.setStartTime(System.currentTimeMillis());
+        viive(3);
+        asiakas1.setEndTime(System.currentTimeMillis());
+
+        //Customer 2
+        Task1_3_Customer asiakas2 = new Task1_3_Customer();
+        asiakas2.setStartTime(System.currentTimeMillis());
+        viive(3);
+        asiakas2.setEndTime(System.currentTimeMillis());
+
+        asiakas1.tulosta();
+        System.out.println("Palveluaika oli " + asiakas1.getDuration());
+        asiakas2.tulosta();
+        System.out.println("Palveluaika oli " + asiakas2.getDuration());
     }
 }
